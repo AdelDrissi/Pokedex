973b801ebde319309034a71a8c784cbd202f14b2
Adel <leda91@hotmail.fr>
2024-02-09 22:43:01 +0100

    axios query to retrieve data and color title

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import {useState} from "react";
-// import {Axios} from "axios";
-
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [pokemonName, setPokemonName] = useState('');
 
-const [setPokemon] = useState("")
+  // REQUETE AXIOS (GET)
+  const searchPokemon = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then((response) => {
+        console.log(response);
+      });
+  };
 
   return (
     <div className="App">
       <div className="titleSection">
-        <h1>Pokemon World</h1>
-        <input type="text"  onChange={(event) => {setPokemon(event.target.value)}}/>
-        <button>Rechercher pokémon</button>
+        <h1 className="poke-world">Pokemon World</h1>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPokemonName(event.target.value);
+          }}
+        />
+        <button onClick={searchPokemon}>Rechercher pokémon</button>
       </div>
     </div>
   );
