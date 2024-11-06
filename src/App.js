7bc3c17e5fafaf5db894bc28d401e58da4fe60d3
Drissi Adel <drissiadel@Drissis-MacBook-Air.local>
2024-11-06 20:01:52 +0100

    ajout de couleur en fonction des stats

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import axios from 'axios';
 import { useState } from 'react';
 
+// Définir les couleurs des types de Pokémon
+const typeColors = {
+  fire: '#f08030',
+  water: '#6890f0',
+  grass: '#78c850',
+  electric: '#f8d030',
+  psychic: '#f85888',
+  ice: '#98d8d8',
+  dragon: '#7038f8',
+  ghost: '#705898',
+  dark: '#705848',
+  fairy: '#ee99ac',
+  fighting: '#c03028',
+  poison: '#a040a0',
+  ground: '#e0c068',
+  flying: '#a890f0',
+  rock: '#b8a038',
+  bug: '#a8b820',
+  normal: '#a8a878',
+  steel: '#b8b8d0',
+};
+
 function App() {
   const [pokemonName, setPokemonName] = useState('');
   const [pokemonChoice, setPokemonChoice] = useState(false);
   const [pokemon, setPokemon] = useState({
-    
     name: '',
     species: '',
     img: '',
@@ -21,6 +42,7 @@ function App() {
       .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
       .then((response) => {
         console.log(response);
+        const type = response.data.types[0].type.name; // Récupérer le type principal du Pokémon
         setPokemon({
           name: pokemonName,
           species: response.data.species.name,
@@ -28,7 +50,7 @@ function App() {
           hp: response.data.stats[0].base_stat,
           attack: response.data.stats[1].base_stat,
           defense: response.data.stats[2].base_stat,
-          type: response.data.types[0].type.name,
+          type,
         });
         setPokemonChoice(true);
       });
@@ -48,16 +70,24 @@ function App() {
       </div>
       <div className="DisplaySection">
         {!pokemonChoice ? (
-          <h1> Choice a pokemon</h1>
+          <h1> Choose a pokemon</h1>
         ) : (
           <>
             <h1>{pokemon.name}</h1>
-            <img src={pokemon.img} />
+            <img src={pokemon.img} alt={`${pokemon.name} sprite`} />
             <h1>Species : {pokemon.species}</h1>
-            <h1>HP : {pokemon.hp}</h1>
-            <h1>defense : {pokemon.defense}</h1>
-            <h1>attack : {pokemon.attack}</h1>
-            <h1>type : {pokemon.type}</h1>
+            <h1 style={{ color: typeColors[pokemon.type] }}>
+              HP : {pokemon.hp}
+            </h1>
+            <h1 style={{ color: typeColors[pokemon.type] }}>
+              Defense : {pokemon.defense}
+            </h1>
+            <h1 style={{ color: typeColors[pokemon.type] }}>
+              Attack : {pokemon.attack}
+            </h1>
+            <h1 style={{ color: typeColors[pokemon.type] }}>
+              Type : {pokemon.type}
+            </h1>
           </>
         )}
       </div>
