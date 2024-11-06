6e4648216ea85178392aa0083cea22bfd5a57b57
Drissi Adel <leda91@hotmail.fr>
2024-11-06 20:25:45 +0100

    slide in animation pour pokemon et stats

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useState } from 'react';
+import './App.css'; // Assurez-vous que le fichier CSS est importé
 
 // Définir les couleurs des types de Pokémon
 const typeColors = {
@@ -35,15 +36,22 @@ function App() {
     defense: '',
     type: '',
   });
+  const [searchCount, setSearchCount] = useState(0); // Nouveau compteur de recherches
+  const [addedPokemons, setAddedPokemons] = useState([]); // Liste des Pokémon ajoutés
 
   // REQUETE AXIOS (GET)
   const searchPokemon = () => {
+    // Vérifier si le Pokémon est déjà dans la liste
+    if (addedPokemons.includes(pokemonName.toLowerCase())) {
+      alert(`${pokemonName} has already been added!`);
+      return;
+    }
+
     axios
       .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
       .then((response) => {
-        console.log(response);
         const type = response.data.types[0].type.name; // Récupérer le type principal du Pokémon
-        setPokemon({
+        const newPokemon = {
           name: pokemonName,
           species: response.data.species.name,
           img: response.data.sprites.front_default,
@@ -51,8 +59,12 @@ function App() {
           attack: response.data.stats[1].base_stat,
           defense: response.data.stats[2].base_stat,
           type,
-        });
+        };
+
+        setPokemon(newPokemon);
         setPokemonChoice(true);
+        setSearchCount((prevCount) => prevCount + 1); // Incrémenter le compteur de recherche
+        setAddedPokemons([...addedPokemons, pokemonName.toLowerCase()]); // Ajouter le Pokémon à la liste
       });
   };
 
@@ -73,19 +85,45 @@ function App() {
           <h1> Choose a pokemon</h1>
         ) : (
           <>
-            <h1>{pokemon.name}</h1>
-            <img src={pokemon.img} alt={`${pokemon.name} sprite`} />
-            <h1>Species : {pokemon.species}</h1>
-            <h1 style={{ color: typeColors[pokemon.type] }}>
+            <h1 key={searchCount} className="slide-in">
+              {pokemon.name}
+            </h1>{' '}
+            {/* Utilisation de searchCount comme clé */}
+            <img
+              key={searchCount}
+              className="slide-in"
+              src={pokemon.img}
+              alt={`${pokemon.name} sprite`}
+            />
+            <h1 key={searchCount} className="slide-in">
+              Species : {pokemon.species}
+            </h1>
+            <h1
+              key={searchCount}
+              className="slide-in"
+              style={{ color: typeColors[pokemon.type] }}
+            >
               HP : {pokemon.hp}
             </h1>
-            <h1 style={{ color: typeColors[pokemon.type] }}>
+            <h1
+              key={searchCount}
+              className="slide-in"
+              style={{ color: typeColors[pokemon.type] }}
+            >
               Defense : {pokemon.defense}
             </h1>
-            <h1 style={{ color: typeColors[pokemon.type] }}>
+            <h1
+              key={searchCount}
+              className="slide-in"
+              style={{ color: typeColors[pokemon.type] }}
+            >
               Attack : {pokemon.attack}
             </h1>
-            <h1 style={{ color: typeColors[pokemon.type] }}>
+            <h1
+              key={searchCount}
+              className="slide-in"
+              style={{ color: typeColors[pokemon.type] }}
+            >
               Type : {pokemon.type}
             </h1>
           </>
